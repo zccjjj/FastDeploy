a6e6b6254c42293f48e735052e3d5a50049e4ceb
zccjjj <zhangcaiji123@163.com>
2026-03-24 20:17:02 +0800

    support C8

--- fastdeploy/model_executor/layers/backends/xpu/attention.py
+++ fastdeploy/model_executor/layers/backends/xpu/attention.py
@@ -181,8 +181,8 @@ class XPUAttentionBackend(AttentionBackend):
         cache_v_scale = getattr(layer, "cache_v_scale", None)
         cache_k_out_scale = getattr(layer, "cache_k_out_scale", None)
         cache_v_out_scale = getattr(layer, "cache_v_out_scale", None)
-        cache_k_zp = getattr(self, "cache_k_zp", None)
-        cache_v_zp = getattr(self, "cache_v_zp", None)
+        cache_k_zp = getattr(layer, "cache_k_zp", None)
+        cache_v_zp = getattr(layer, "cache_v_zp", None)
 
         if layer.use_qk_norm:
             q_norm_weight = layer.q_norm_weight
@@ -219,10 +219,10 @@ class XPUAttentionBackend(AttentionBackend):
             forward_meta.prefix_len,
             cache_k_scale,
             cache_v_scale,
-            cache_k_out_scale,
-            cache_v_out_scale,
-            cache_k_zp,
-            cache_v_zp,
+            cache_k_out_scale.astype("bfloat16") if cache_k_out_scale is not None else None, # for C8
+            cache_v_out_scale.astype("bfloat16") if cache_v_out_scale is not None else None, # for C8
+            cache_k_zp.astype("bfloat16") if cache_k_zp is not None else None, # for C8
+            cache_v_zp.astype("bfloat16") if cache_v_zp is not None else None, # for C8
             None,  # shift
             None,  # smooth
             q_norm_weight,
